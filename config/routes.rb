7b45916527864ffb43a20f3435b23f6422655b09
shos3 <sho63.baseball@gmail.com>
2021-08-23 10:30:04 +0900

    計画ページ1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   root 'application#index'
   resources :application
   resources :plan, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  get "plan/show"  
 end
 
