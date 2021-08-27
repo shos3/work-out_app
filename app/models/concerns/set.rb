eac801641668457836020af0e076e99e2fb32343
shos3 <sho63.baseball@gmail.com>
2021-08-27 18:47:20 +0900

    不要ファイル削除

--- app/models/concerns/set.rb
+++
@@ -1,9 +0,0 @@
-class Set < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '1' }, { id: 3, name: '2' }, { id: 4, name: '3' }, { id: 4, name: '4' }, { id: 4, name: '5' },
-    { id: 4, name: '6' }, { id: 4, name: '7' }, { id: 4, name: '8' }, { id: 4, name: '9' }, { id: 4, name: '10' }
-  ]
-  include ActiveHash::Associations
-  has_many :plans
-end
