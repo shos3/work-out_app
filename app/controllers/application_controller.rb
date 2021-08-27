5570e64811fc7920beb3699e848170f03a52a765
shos3 <sho63.baseball@gmail.com>
2021-08-27 19:27:53 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   def after_inactive_sign_up_path_for(_resource)
     application_plan_index_path(application_id)
@@ -9,6 +11,16 @@ class ApplicationController < ActionController::Base
   def show; end
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
+
+
+
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname age])
