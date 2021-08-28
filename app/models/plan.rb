55323e1668ecb8a61fe44aa9501434eb243c9e84
shos3 <sho63.baseball@gmail.com>
2021-08-28 10:32:33 +0900

    訂正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,7 +5,7 @@ class Plan < ApplicationRecord
   validates :number, presence: true
   validates :set, presence: true
   validates :time, presence: true
-  validate :memo,allow_blank: true
+  validate :memo
   validates :start_time, presence: true
 
   belongs_to :user
