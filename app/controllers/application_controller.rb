36393be380d778041ca8588c51532f986a315dd8
shos3 <sho63.baseball@gmail.com>
2021-08-31 14:07:39 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
 
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   #def configure_permitted_parameters
-   # devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar))
+ # devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar))
  # end
 
 end
